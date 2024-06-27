50eb66e7e096b9419c80e675bb96539e7e911c5b
Naman Kumar <naman.kumardmp@gmail.com>
2024-06-27 23:16:14 +0530

    modify

--- Building server/main.js
+++ Building server/main.js
@@ -2,9 +2,19 @@ const http = require ("http");
 const fs = require ("fs")
 const myserver = http.createServer((req,res) => {
     const now = new Date();
-    const log =`Day-> ${Date.now()}  Time-> ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} :- New request received \n`;
+    const log =`Day-> ${Date.now()}  Time-> ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}  ${req.url} :- New request received \n`;
     fs.appendFile("log.txt",log,(err,data) => {
-        res.end("hello from server again")
+        switch(req.url){
+            case "/":
+                res.end("home page")
+                break;
+            case "/about":
+                res.end("I am Naman")
+                break;
+            default :
+            res.end("404 not found") 
+        }
+        
     })
 })
 
